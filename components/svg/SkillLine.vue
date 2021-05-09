08d1c57618c5d2ec2538d8f7a3d5d86b3c282d1c
ImLuckyJr <im.lucky.jr@gmail.com>
2021-05-10 02:16:15 +0500

    Правки

---
+++ components/svg/SkillLine.vue
@@ -0,0 +1,26 @@
+<template>
+    <svg
+        fill="none"
+        height="689"
+        viewBox="0 0 840 689"
+        width="840"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M615.001 2.999C615.001 2.999 913.204 -19.062 819.001 192C779.501 280.5 887.501 625.5 727.001 607.5C638.243 597.546 692.49 251.478 464.001 473.5C411.001 525 336.501 638 250.001 601.5M24.5015 546C143.001 621.529 146.001 575 250.001 601.5M250.001 601.5C154.501 577.166 146.501 704 1.00146 685"
+            stroke="#F76D6D"
+            stroke-dasharray="25 15"
+            stroke-width="4"
+        />
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'skill-line',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/svg/StepsMultiDots.vue
@@ -0,0 +1,93 @@
+<template>
+    <svg
+        fill="none"
+        height="93"
+        viewBox="0 0 93 93"
+        width="93"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <ellipse
+            cx="22.5824"
+            cy="32.6495"
+            fill="#44AE65"
+            rx="6.81478"
+            ry="6.73715"
+            transform="rotate(29.64 22.5824 32.6495)"
+        />
+        <ellipse
+            cx="56.6396"
+            cy="83.0338"
+            fill="#44AE65"
+            rx="6.81478"
+            ry="6.73715"
+            transform="rotate(29.64 56.6396 83.0338)"
+        />
+        <ellipse
+            cx="32.9472"
+            cy="69.5529"
+            fill="#44AE65"
+            rx="6.81478"
+            ry="6.73715"
+            transform="rotate(29.64 32.9472 69.5529)"
+        />
+        <ellipse
+            cx="9.25479"
+            cy="56.0718"
+            fill="#44AE65"
+            rx="6.81478"
+            ry="6.73715"
+            transform="rotate(29.64 9.25479 56.0718)"
+        />
+        <ellipse
+            cx="83.2948"
+            cy="36.188"
+            fill="#44AE65"
+            rx="6.81478"
+            ry="6.73715"
+            transform="rotate(29.64 83.2948 36.188)"
+        />
+        <ellipse
+            cx="59.6027"
+            cy="22.7071"
+            fill="#44AE65"
+            rx="6.81478"
+            ry="6.73715"
+            transform="rotate(29.64 59.6027 22.7071)"
+        />
+        <ellipse
+            cx="35.9103"
+            cy="9.22608"
+            fill="#44AE65"
+            rx="6.81478"
+            ry="6.73715"
+            transform="rotate(29.64 35.9103 9.22608)"
+        />
+        <ellipse
+            cx="69.9669"
+            cy="59.6115"
+            fill="#44AE65"
+            rx="6.81478"
+            ry="6.73715"
+            transform="rotate(29.64 69.9669 59.6115)"
+        />
+        <ellipse
+            cx="46.2746"
+            cy="46.1305"
+            fill="#44AE65"
+            rx="6.81478"
+            ry="6.73715"
+            transform="rotate(29.64 46.2746 46.1305)"
+        />
+    </svg>
+
+</template>
+
+<script>
+export default {
+    name: 'StepsMultiDots',
+};
+</script>
+
+<style scoped>
+
+</style>
