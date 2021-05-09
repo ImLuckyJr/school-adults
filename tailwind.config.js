39a6e9d93adc3393475dad229e977ae24b09175d
ImLuckyJr <im.lucky.jr@gmail.com>
2021-05-09 23:12:57 +0500

    Start

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge:    [],
+    darkMode: false, // or 'media' or 'class'
+    theme:    {
+        extend: {
+            colors: {
+                'pr-red': '#F76D6D',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins:  [],
+};
