9277356e04836897c5b29e674348b68995712e87
RubanZ <ruban_1998@hotmail.com>
2021-07-12 14:44:19 +0500

    Пародия на разделы

---
+++ components/CourseElement/Sections.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="">
+    <div class="tab__header">
+      <a
+        href="#"
+        class="
+          tab__link
+          p-4
+          block
+          bg-blue-dark
+          hover:bg-blue-darker
+          no-underline
+          text-white
+          border-b-2 border-white
+          flex
+          justify-between
+        "
+        @click.prevent="active = !active"
+      >
+        <p class="course__info__title">{{ title }}</p>
+        <span class="down-Arrow" v-show="!active">&#9660;</span>
+        <span class="up-Arrow" v-show="active">&#9650;</span>
+      </a>
+    </div>
+    <div class="course__info__description" v-show="active"><slot /></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sections",
+  props: ["title"],
+  data() {
+    return {
+      active: false,
+    }
+  }
+};
+</script>
+
+<style scope>
+</style>
--- pages/courses/_id/index.vue
+++ pages/courses/_id/index.vue
@@ -1,109 +1,146 @@
 <template>
-    <div>
-        <section
-            id="course"
-            class="section tw-mt-16 courses"
+  <div>
+    <section id="course" class="section tw-mt-16 courses">
+      <div
+        :class="!form ? 'tw-animate-pulse tw-bg-pr-blue tw-h-16' : ''"
+        :style="!form ? 'border-radius: 16px;' : ''"
+        class="title title--left"
+      >
+        <h2>{{ form ? form.title : "" }}</h2>
+      </div>
+
+      <div v-if="form" class="courses__body">
+        <div class="course__wrapper course__wrapper--full">
+          <div
+            class="course__avatar"
+            v-bind:style="{
+              'background-image':
+                'url(' + $axios.defaults.baseURL + form.avatar + ')',
+            }"
+          />
+          <div class="course__info">
+            <div class="course__info__bg" />
+            <div class="course__info__title">Аннотация курса</div>
+            <div class="course__info__description">
+              {{ form.description }}
+            </div>
+            <div class="course__info__status">
+              <div class="users">
+                {{ form.users ? form.users : 505 }}
+              </div>
+              <div class="rating">{{ form.rating }}</div>
+              <div class="time">{{ form.difficulty }}</div>
+            </div>
+            <div class="course__info__likes">
+              {{ form.likes ? form.likes : 123 }}
+            </div>
+          </div>
+
+          <div class="course__action">
+            <ButtonDefault align="right" class="btn--default">
+              <NuxtLink :to="`/courses/${$route.params.id}/learn`"
+                >Перейти</NuxtLink
+              >
+            </ButtonDefault>
+          </div>
+        </div>
+      </div>
+      <div
+        v-else
+        class="courses__body tw-flex tw-items-center tw-animate-pulse"
+      >
+        <div class="course__wrapper course__wrapper--full">
+          <div class="course__avatar" />
+          <div class="course__info">
+            <div class="course__info__bg" />
+            <div class="course__info__title" />
+            <div class="course__info__description" />
+            <div class="course__info__status" />
+            <div class="course__info__likes" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="course__sections" class="section tw-mt-16 courses">
+      <div v-if="page" class="courses__body tw-space-y-6">
+        <h2 class="tw-text-center">Разделы курса</h2>
+        <div
+          class="
+            tw-grid tw-items-center tw-justify-center tw-grid-cols-2 tw-gap-4
+          "
         >
-            <div
-                :class="(!form) ? 'tw-animate-pulse tw-bg-pr-blue tw-h-16' : ''"
-                :style="(!form) ? 'border-radius: 16px;' : ''"
-                class="title  title--left"
-            >
-                <h2>{{ (form) ? form.title : '' }}</h2>
-            </div>
-            
-            <div
-                v-if="form"
-                class="courses__body"
-            >
-                <div class="course__wrapper course__wrapper--full">
-                    <div
-                        class="course__avatar"
-                        v-bind:style="{
-                            'background-image': 'url(' + $axios.defaults.baseURL + form.avatar + ')',
-                        }"
-                    />
-                    <div class="course__info">
-                        <div class="course__info__bg"/>
-                        <div class="course__info__title">Аннотация курса</div>
-                        <div class="course__info__description">
-                            {{ form.description }}
-                        </div>
-                        <div class="course__info__status">
-                            <div class="users">
-                                {{ form.users ? form.users : 505 }}
-                            </div>
-                            <div class="rating">{{ form.rating }}</div>
-                            <div class="time">{{ form.difficulty }}</div>
-                        </div>
-                        <div class="course__info__likes">
-                            {{ form.likes ? form.likes : 123 }}
-                        </div>
-                    </div>
-                    
-                    <div class="course__action">
-                        <ButtonDefault
-                            align="right"
-                            class="btn--default"
-                        >
-                            <NuxtLink :to="`/courses/${ $route.params.id }/learn`">Перейти</NuxtLink>
-                        </ButtonDefault>
-                    </div>
-                </div>
-            </div>
-            <div
-                v-else
-                class="courses__body tw-flex tw-items-center tw-animate-pulse"
-            >
-                <div class="course__wrapper course__wrapper--full">
-                    <div class="course__avatar"/>
-                    <div class="course__info">
-                        <div class="course__info__bg"/>
-                        <div class="course__info__title"/>
-                        <div class="course__info__description"/>
-                        <div class="course__info__status"/>
-                        <div class="course__info__likes"/>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
+          <template v-for="section in page.sections">
+            <CourseElementSections :key="section.id" :title="section.title">
+              <div v-html="section.description"></div>
+            </CourseElementSections>
+          </template>
+        </div>
+      </div>
+    </section>
+    <section class="section tw-mt-16 courses">
+      <div v-if="page" class="courses__body tw-space-y-6">
+        <h3>Данные</h3>
+        {{ page }}
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'CourseIdIndex',
-    data() {
-        return {
-            form: null,
-        };
-    },
-    // call fetch only on client-side
-    // TODO: УБРАТЬ!!!
-    fetchOnServer: false,
-    async fetch() {
-        try {
-            const response = await this.$axios.$get(this.$axios.defaults.baseURL + 'courses/get/' + this.$route.params.id);
-            console.log('response');
-            console.log(response);
-            this.form = response;
-        }
-        catch (err) {
-            console.log('ERROR GET ALL COURSES');
-            console.error(err);
-            this.$toast.error('Произошла ошибка при получении курсов. Попробуйте позже или обратитесь в техподдержку.');
-        }
-    },
-    mounted() {
-        this.$nextTick(() => {
-            this.$nuxt.$loading.start();
-            
-            setTimeout(() => this.$nuxt.$loading.finish(), 500);
-        });
-    },
+  name: "CourseIdIndex",
+  data() {
+    return {
+      form: null,
+      page: null,
+    };
+  },
+  // call fetch only on client-side
+  // TODO: УБРАТЬ!!!
+  fetchOnServer: false,
+  async fetch() {
+    try {
+      const response = await this.$axios.$get(
+        this.$axios.defaults.baseURL + "courses/get/" + this.$route.params.id
+      );
+      console.log("response");
+      console.log(response);
+      this.form = response;
+    } catch (err) {
+      console.log("ERROR GET ALL COURSES");
+      console.error(err);
+      this.$toast.error(
+        "Произошла ошибка при получении курсов. Попробуйте позже или обратитесь в техподдержку."
+      );
+    }
+
+    try {
+      const response = await this.$axios.$get(
+        this.$axios.defaults.baseURL +
+          "courses/get/" +
+          this.$route.params.id +
+          "/about"
+      );
+      console.log("response");
+      console.log(response);
+      this.page = response;
+    } catch (err) {
+      console.log("ERROR GET ALL COURSES");
+      console.error(err);
+      this.$toast.error(
+        "Произошла ошибка при получении страницы. Попробуйте позже или обратитесь в техподдержку."
+      );
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start();
+
+      setTimeout(() => this.$nuxt.$loading.finish(), 500);
+    });
+  },
 };
 </script>
 
 <style scoped>
-
 </style>
